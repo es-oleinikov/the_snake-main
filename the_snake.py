2668a63debb0a8f1754a3f93e7ac96de774c66a1
Oleinikov <j-n-d@yandex.ru>
2024-02-14 13:46:24 +0300

    Добавлено изменене скорости

--- the_snake.py
+++ the_snake.py
@@ -2,14 +2,12 @@ from random import choice, randint
 
 import pygame
 
-# Инициализация PyGame:
-pygame.init()
-
 # Константы для размеров поля и сетки:
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
+START_POSITION = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
 # Направления движения:
 UP = (0, -1)
@@ -29,6 +27,9 @@ APPLE_COLOR = (255, 0, 0)
 # Цвет змейки
 SNAKE_COLOR = (0, 255, 0)
 
+# Базовый цвет
+BASE_COLOR = (0, 0, 0)
+
 # Скорость движения змейки:
 SPEED = 20
 
@@ -45,9 +46,7 @@ clock = pygame.time.Clock()
 class GameObject:
     """Базовый класс."""
 
-    start_position = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-
-    def __init__(self, body_color=(0, 0, 0), position=start_position) -> None:
+    def __init__(self, body_color=BASE_COLOR, position=START_POSITION):
         """Конструктор класса GameObject."""
         self.position = position
         self.body_color = body_color
@@ -60,17 +59,15 @@ class GameObject:
 class Snake(GameObject):
     """Класс, описывающий змейку."""
 
-    def __init__(self, body_color=SNAKE_COLOR):
+    def __init__(self, body_color=SNAKE_COLOR, position=START_POSITION):
         """Конструктор класса Snake."""
-        super().__init__(body_color)
+        super().__init__(body_color, position)
         self.length = 1
-        self.position = GameObject.start_position
+        self.position = position
         self.positions = [self.position]
         self.direction = RIGHT
         self.next_direction = None
         self.last = None
-
-    # Метод обновления направления после нажатия на кнопку
 
     def update_direction(self):
         """Обновляет направление движения змейки."""
@@ -136,7 +133,7 @@ class Snake(GameObject):
 class Apple(GameObject):
     """Класс описывает Яблоко."""
 
-    def __init__(self, body_color=APPLE_COLOR):
+    def __init__(self, body_color=APPLE_COLOR, position=START_POSITION):
         """Конструктор класса Apple."""
         super().__init__(body_color)
         self.position = self.randomize_position()
@@ -177,9 +174,11 @@ def handle_keys(game_object):
 
 def main():
     """Обрабатывает основной цикл игры."""
+    pygame.init()
     snake = Snake(SNAKE_COLOR)
     apple = Apple(APPLE_COLOR)
     while True:
+        record = 0
         clock.tick(SPEED)
         handle_keys(snake)
         snake.update_direction()
@@ -187,12 +186,14 @@ def main():
         snake.draw(screen)
         apple.draw(screen)
         if apple.position in snake.positions:
+            record += 1
             snake.length += 1
             apple.randomize_position()
             while apple.position in snake.positions:
                 apple.randomize_position()
             apple.draw(screen)
         pygame.display.update()
+        print(record)
 
 
 if __name__ == '__main__':
